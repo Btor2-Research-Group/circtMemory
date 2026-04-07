a861f4c648caf2e90d2abc5dcb82e4dbc9cab76b
rsingh135 <rsingh.ala03@gmail.com>
2026-04-06 22:29:07 -0400

    Add core functionality for UndefinedMemoryBehavior pass

--- lib/Dialect/Seq/Transforms/UndefinedMemoryBehavior.cpp
+++ lib/Dialect/Seq/Transforms/UndefinedMemoryBehavior.cpp
@@ -7,12 +7,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "circt/Dialect/Comb/CombOps.h"
+#include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/Seq/SeqOps.h"
 #include "circt/Dialect/Seq/SeqPasses.h"
-#include "circt/Support/BackedgeBuilder.h"
+#include "mlir/IR/ImplicitLocOpBuilder.h"
 #include "mlir/Pass/Pass.h"
-#include "mlir/Transforms/DialectConversion.h"
-#include "llvm/ADT/TypeSwitch.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/SmallVector.h"
 
 namespace circt {
 namespace seq {
@@ -23,8 +24,18 @@ namespace seq {
 
 using namespace circt;
 using namespace seq;
+using namespace hw;
 
 namespace {
+
+struct HashMapInstances {
+    // The memory instance itself
+    seq::FirMemOp memOp;
+    // All the read, write, and readwrite operations that are using this memory instance
+    llvm::SmallVector<seq::FirMemReadOp, 4> reads;
+    llvm::SmallVector<seq::FirMemWriteOp, 4> writes;
+    llvm::SmallVector<seq::FirMemReadWriteOp, 4> readWrites;
+};
 
 /// Lowers seq.compreg.ce to a seq.compreg with the clock enable signal
 /// built into the next logic, i.e. `next := mux(clock_enable, next, current)`
@@ -39,4 +50,101 @@ public:
 
 void UndefinedMemoryBehavior::runOnOperation() {
   // The two for loop place to code
+  auto module = getOperation();
+
+    // Set up hashmap for the SRAM instances
+    llvm::SmallDenseMap<Value, HashMapInstances> sramMap;
+
+    // Initialize the SRAM hashmap
+    module.walk([&](seq::FirMemOp op) {
+        Value sramResult = op.getResult();
+        sramMap[sramResult].memOp = op;
+
+        // Find all the read, write, and readwrite operations that are using this memory and add them to the hashmap at the same key
+        // Users vs Uses? 
+        for (Operation *user : sramResult.getUsers()) {
+            llvm::TypeSwitch<Operation *>(user)
+                .Case<seq::FirMemReadOp>([&](seq::FirMemReadOp readOp) {sramMap[sramResult].reads.push_back(readOp);})
+                .Case<seq::FirMemWriteOp>([&](seq::FirMemWriteOp writeOp) {sramMap[sramResult].writes.push_back(writeOp);})
+                .Case<seq::FirMemReadWriteOp>([&](seq::FirMemReadWriteOp readWriteOp) {sramMap[sramResult].readWrites.push_back(readWriteOp);});
+        }
+
+    });
+
+    for (auto &object : sramMap) {
+        HashMapInstances instance = object.second;
+        auto readOps = instance.reads;
+        auto writeOps = instance.writes;
+        auto readWriteOps = instance.readWrites;
+        int randomCounter = 0;
+
+        // If either list is empty we can return early
+        if (readOps.empty() || (writeOps.empty() && readWriteOps.empty())) {
+            continue;
+        }
+
+        // Add the mutually exclusive check when you find a write or readWrite (before checking Enabled)
+        // With multiple writes (or reads) in the same cycle, you need to pick one that happens in this cycle and then
+        // buffer to store the writes and delay them for later cycles (check for a FIFO buffer operation)
+
+        // Use the FIRTL kevin pass with multiple writes in the same cycle, and see if it generates a buffer
+        // If it does then we need to implement above ^ 
+
+        // Loop through all the read and write ports and check if they are accessing the same address
+        for (auto readOp : readOps) {
+            ImplicitLocOpBuilder b(module.getLoc(), module.getBody());
+            b.setInsertionPointAfter(readOp);
+            // Maintain a list of the actual collisions that we can later use in the mux. 
+            SmallVector<Value, 16> collisionList;
+
+            for (auto writeOp : writeOps) {
+                auto isSameAddress = b.create<comb::ICmpOp>(comb::ICmpPredicate::eq, readOp.getAddress(), writeOp.getAddress());
+
+                // If they are the same address, we need to ensure they are going to collide
+                Value readIsEnabled = readOp.getEnable();
+                Value writeIsEnabled = writeOp.getEnable();
+                Value readAndWriteEnabled = b.create<comb::AndOp>(readIsEnabled, writeIsEnabled);
+                Value isCollision = b.create<comb::AndOp>(isSameAddress, readAndWriteEnabled);
+
+                // Add this collision to the list of collisions for this read operation
+                collisionList.push_back(isCollision);
+            }
+            
+            // Check the ReadWrite ports as well
+            for (auto readWriteOp : readWriteOps) {
+                auto isSameAddress = b.createOrFold<comb::ICmpOp>(comb::ICmpPredicate::eq, readOp.getAddress(), readWriteOp.getAddress());
+
+                // If they are the same address, we need to ensure they are going to collide
+                Value readIsEnabled = readOp.getEnable();
+                Value writeIsEnabled = readWriteOp.getEnable();
+                Value readAndWriteEnabled = b.create<comb::AndOp>(readIsEnabled, writeIsEnabled);
+                Value isCollision = b.create<comb::AndOp>(isSameAddress, readAndWriteEnabled);
+
+                // Add this collision to the list of collisions for this read operation
+                collisionList.push_back(isCollision);
+            }
+
+            if (collisionList.empty()) {
+                continue;
+            }
+
+            // Use createOrFold in case there is only one collision to avoid unnecessary logic
+            Value conflictTrue = b.createOrFold<comb::OrOp>(mlir::ValueRange(collisionList), false);
+
+            // Create a random value to use for undefined behavior. We tell the solver to randomly choose a value. 
+            std::string randomName = "randomValueForUndefinedBehavior" + std::to_string(randomCounter++);
+            auto randomPair = module.appendInput(b.getStringAttr(randomName), readOp.getType());
+            // Use symbolic value instead
+            Verif::SymbolicValue.op
+            Value randomVal = randomPair.second;
+
+            // If true, we have a read-write collision and we can enable undefined memory behavior
+            // This mux chooses between the correct value and an undefined value based on whether there is a collision or not
+            Value mux = b.create<comb::MuxOp>(conflictTrue, randomVal, readOp.getResult());
+
+            Operation *muxOp = mux.getDefiningOp();
+            readOp.getResult().replaceAllUsesExcept(mux, muxOp);
+            
+        }
+    }
 }
