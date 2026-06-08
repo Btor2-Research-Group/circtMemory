fc21902cf01d908aca1ae9349771e505cd4c51b8
A. Thompson <at5828@princeton.edu>
2026-06-08 18:04:02 -0400

    Avery work so far.Working on mlir checks, before moving to writes.

--- lib/Dialect/Seq/Transforms/UndefinedMemoryBehavior.cpp
+++ lib/Dialect/Seq/Transforms/UndefinedMemoryBehavior.cpp
@@ -10,11 +10,12 @@
 #include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/Seq/SeqOps.h"
 #include "circt/Dialect/Seq/SeqPasses.h"
+#include "circt/Dialect/Verif/VerifOps.h"
+#include "circt/Support/Namespace.h"
 #include "mlir/IR/ImplicitLocOpBuilder.h"
 #include "mlir/Pass/Pass.h"
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/SmallVector.h"
-#include "circt/Dialect/Verif/VerifOps.h"
 
 namespace circt {
 namespace seq {
@@ -24,27 +25,35 @@ namespace seq {
 } // namespace circt
 
 using namespace circt;
-using namespace seq;
-using namespace hw;
+using namespace mlir;
+using namespace llvm;
+// Namespace: scope for structures & functions
+
+// Already used: can comment out
+// using namespace seq;
+// using namespace hw;
 
 namespace {
 
-struct HashMapInstances {
-    // The memory instance itself
-    seq::FirMemOp memOp;
-    // All the read, write, and readwrite operations that are using this memory instance
-    llvm::SmallVector<seq::FirMemReadOp, 4> reads;
-    llvm::SmallVector<seq::FirMemWriteOp, 4> writes;
-    llvm::SmallVector<seq::FirMemReadWriteOp, 4> readWrites;
+struct RWMap {
+  // The memory instance itself
+  seq::FirMemOp memOp;
+  // All the read, write, and readwrite operations that are using this memory
+  // instance
+  llvm::SmallVector<seq::FirMemReadOp, 4> reads;
+  llvm::SmallVector<seq::FirMemWriteOp, 4> writes;
+  llvm::SmallVector<seq::FirMemReadWriteOp, 4> readWrites;
 };
 
 /// Lowers seq.compreg.ce to a seq.compreg with the clock enable signal
 /// built into the next logic, i.e. `next := mux(clock_enable, next, current)`
 struct UndefinedMemoryBehavior
-: seq::impl::UndefinedMemoryBehaviorBase<UndefinedMemoryBehavior> {
+    : seq::impl::UndefinedMemoryBehaviorBase<UndefinedMemoryBehavior> {
 public:
   void runOnOperation() override;
-  
+
+private:
+  Namespace symbolNamespace;
 };
 
 } // namespace
@@ -52,114 +61,298 @@ public:
 void UndefinedMemoryBehavior::runOnOperation() {
   auto module = getOperation();
 
-    // Set up hashmap for the SRAM instances
-    llvm::SmallDenseMap<Value, HashMapInstances> sramMap;
+  // Set up hashmap for the SRAM instances
+  // TODO : switch to
+  // SmallDenseMap<Value, SmallDenseMap>
+  llvm::SmallDenseMap<Value, RWMap> sramMap;
 
-    // Initialize the SRAM hashmap
-    module.walk([&](seq::FirMemOp op) {
-        Value sramResult = op.getResult();
-        sramMap[sramResult].memOp = op;
+  // Initialize the SRAM hashmap
+  module.walk([&](seq::FirMemOp op) {
+    // Sram Result is a SSA value
+    // representing a memory operation.
 
-        // Find all the read, write, and readwrite operations that are using this memory and add them to the hashmap at the same key
-        for (Operation *user : sramResult.getUsers()) {
-            llvm::TypeSwitch<Operation *>(user)
-                .Case<seq::FirMemReadOp>([&](seq::FirMemReadOp readOp) {sramMap[sramResult].reads.push_back(readOp);})
-                .Case<seq::FirMemWriteOp>([&](seq::FirMemWriteOp writeOp) {sramMap[sramResult].writes.push_back(writeOp);})
-                .Case<seq::FirMemReadWriteOp>([&](seq::FirMemReadWriteOp readWriteOp) {sramMap[sramResult].readWrites.push_back(readWriteOp);});
-        }
+    // Instance of a syntactic object
+    // THE memory, not just a pointer. Physical block
+    Value sramResult = op.getResult();
+    sramMap[sramResult].memOp = op;
 
-    });
-
-    for (auto &object : sramMap) {
-        HashMapInstances instance = object.second;
-        auto readOps = instance.reads;
-        auto writeOps = instance.writes;
-        auto readWriteOps = instance.readWrites;
-        int randomCounter = 0;
-
-        // If either list is empty we can return early
-        if (readOps.empty() || (writeOps.empty() && readWriteOps.empty())) {
-            continue;
-        }
-
-        // Loop through all the read and write ports and check if they are accessing the same address
-        for (auto readOp : readOps) {
-            ImplicitLocOpBuilder b(module.getLoc(), module.getBody());
-            b.setInsertionPointAfter(readOp);
-
-            // Out of Bounds checker
-            Value currentResult = readOp.getResult();
-            uint64_t depth = instance.memOp.getMemory().getType().getDepth();
-            if (depth > 0) {
-                Value addr = readOp.getAddress();
-                Value depthValue = b.create<hw::ConstantOp>(addr.getType(), depth);
-
-                // Hazard if: (Address >= Depth) which means we are out of bounds and can have undefined behavior
-                // Use a symbolic value so at runtime the value is chosen nondeterministically
-                Value isOutOfBounds = b.createOrFold<comb::ICmpOp>(comb::ICmpPredicate::uge, addr, depthValue);
-
-                std::string oobName = "randomValueForOOB" + std::to_string(randomCounter++);
-                auto randomSymbolicOOB = verif::SymbolicValueOp::create(b, currentResult.getType(), b.getStringAttr(oobName));
-                Value randomOOBVal = randomSymbolicOOB.getResult();
-
-                Value muxForOOB = b.create<comb::MuxOp>(isOutOfBounds, randomOOBVal, currentResult);
-                Operation *muxOOBOp = muxForOOB.getDefiningOp();
-                currentResult.replaceAllUsesExcept(muxForOOB, muxOOBOp);
-
-                // Update currentResult so later logic uses the OOB-protected value.
-                currentResult = muxForOOB;
-            }
-
-            // Maintain a list of the actual collisions that we can later use in the mux. 
-            SmallVector<Value, 16> collisionList;
-
-            for (auto writeOp : writeOps) {
-                auto isSameAddress = b.create<comb::ICmpOp>(comb::ICmpPredicate::eq, readOp.getAddress(), writeOp.getAddress());
-
-                // If they are the same address, we need to ensure they are going to collide
-                Value readIsEnabled = readOp.getEnable();
-                Value writeIsEnabled = writeOp.getEnable();
-                Value readAndWriteEnabled = b.create<comb::AndOp>(readIsEnabled, writeIsEnabled);
-                Value isCollision = b.create<comb::AndOp>(isSameAddress, readAndWriteEnabled);
-
-                // Add this collision to the list of collisions for this read operation
-                collisionList.push_back(isCollision);
-            }
-            
-            // Check the ReadWrite ports as well
-            for (auto readWriteOp : readWriteOps) {
-                auto isSameAddress = b.createOrFold<comb::ICmpOp>(comb::ICmpPredicate::eq, readOp.getAddress(), readWriteOp.getAddress());
-
-                // If they are the same address, we need to ensure they are going to collide
-                Value readIsEnabled = readOp.getEnable();
-                Value writeIsEnabled = readWriteOp.getEnable();
-                Value readAndWriteEnabled = b.create<comb::AndOp>(readIsEnabled, writeIsEnabled);
-                Value isCollision = b.create<comb::AndOp>(isSameAddress, readAndWriteEnabled);
-
-                // Add this collision to the list of collisions for this read operation
-                collisionList.push_back(isCollision);
-            }
-
-            if (collisionList.empty()) {
-                continue;
-            }
-
-            // Use createOrFold in case there is only one collision to avoid unnecessary logic
-            Value conflictTrue = b.createOrFold<comb::OrOp>(mlir::ValueRange(collisionList), false);
-
-            // Create a symbolic value to use for undefined behavior so it's chosen at runtime
-            std::string randomName = "randomValueForUndefinedBehavior" + std::to_string(randomCounter++);
-            auto randomSymbolic = verif::SymbolicValueOp::create(b, readOp.getType(), b.getStringAttr(randomName));
-            Value randomVal = randomSymbolic.getResult();
-
-            // If true, we have a read-write collision and we can enable undefined memory behavior
-            // This mux chooses between the correct value and an undefined value based on whether there is a collision or not
-            // This also upholds the OOB and return the random OOB value if we are out of bounds regardless of collisions
-            Value mux = b.create<comb::MuxOp>(conflictTrue, randomVal, currentResult);
-
-            Operation *muxOp = mux.getDefiningOp();
-            readOp.getResult().replaceAllUsesExcept(mux, muxOp);
-            
-        }
+    // Find all the read, write, and readwrite operations that are using this
+    // memory and add them to the hashmap at the same key
+    for (Operation *user : sramResult.getUsers()) {
+      llvm::TypeSwitch<Operation *>(user)
+          .Case<seq::FirMemReadOp>([&](seq::FirMemReadOp readOp) {
+            sramMap[sramResult].reads.push_back(readOp);
+          })
+          .Case<seq::FirMemWriteOp>([&](seq::FirMemWriteOp writeOp) {
+            sramMap[sramResult].writes.push_back(writeOp);
+          })
+          .Case<seq::FirMemReadWriteOp>(
+              [&](seq::FirMemReadWriteOp readWriteOp) {
+                sramMap[sramResult].readWrites.push_back(readWriteOp);
+              })
+          .Default([](auto) {});
     }
+  });
+
+  // TODO: Switch to OpBuilder
+  // mlir::
+  ImplicitLocOpBuilder b(module.getLoc(), module.getBody());
+
+  //----------------
+  // Initialization complete.
+
+  // Iterate through all keys
+  for (auto &object : sramMap) {
+    // Iterate through the hashmaps of each key
+    RWMap instance = object.second;
+    auto readOps = instance.reads;
+    auto writeOps = instance.writes;
+    auto readWriteOps = instance.readWrites;
+
+    // If either list is empty we can return early
+    if (readOps.empty() || (writeOps.empty() && readWriteOps.empty())) {
+      continue;
+    }
+
+    // Loop through all the read and write ports and check if they are accessing
+    // the same address
+    for (auto readOp : readOps) {
+      // TODO: check
+      b.setInsertionPointAfter(readOp);
+
+      // Out of Bounds checker
+      Value currentResult = readOp.getResult();
+      // Width of the address?
+      // Width greater than supported?
+      uint64_t depth = instance.memOp.getMemory().getType().getDepth();
+
+      // Check if empty.
+      if (depth > 0) {
+        Value addr = readOp.getAddress();
+        Value depthValue = b.create<hw::ConstantOp>(addr.getType(), depth);
+        
+
+        // Hazard if: (Address >= Depth) which means we are out of bounds and
+        // can have undefined behavior Use a symbolic value so at runtime the
+        // value is chosen nondeterministically
+        Value isOutOfBounds = b.createOrFold<comb::ICmpOp>(
+            comb::ICmpPredicate::uge, addr, depthValue);
+
+        
+        // Randomize if needed
+        auto oobName = symbolNamespace.newName("randomValueForOOB");
+
+        // Aka choice, but used differently in application
+        auto randomSymbolicOOB = verif::SymbolicValueOp::create(
+            b, currentResult.getType(), b.getStringAttr(oobName));
+        Value randomOOBVal = randomSymbolicOOB.getResult();
+
+        Value muxForOOB =
+            b.create<comb::MuxOp>(isOutOfBounds, randomOOBVal, currentResult);
+        Operation *muxOOBOp = muxForOOB.getDefiningOp();
+
+          /*
+         Value muxForOOB =
+            b.create<comb::MuxOp>(isOutOfBounds, randomOOBVal, currentResult);
+        Operation *muxOOBOp = muxForOOB.getDefiningOp();
+        */
+        currentResult.replaceAllUsesExcept(muxForOOB, muxOOBOp);
+
+        // Update currentResult so later logic uses the OOB-protected value.
+        currentResult = muxForOOB;
+      }
+
+      // Maintain a list of the actual collisions that we can later use in the
+      // mux.
+      SmallVector<Value, 16> collisionList;
+
+      for (auto writeOp : writeOps) {
+        auto isSameAddress = b.create<comb::ICmpOp>(
+            comb::ICmpPredicate::eq, readOp.getAddress(), writeOp.getAddress());
+
+        // If they are the same address, we need to ensure they are going to
+        // collide
+        Value readIsEnabled = readOp.getEnable();
+        Value writeIsEnabled = writeOp.getEnable();
+        Value readAndWriteEnabled =
+            b.create<comb::AndOp>(readIsEnabled, writeIsEnabled);
+        Value isCollision =
+            b.create<comb::AndOp>(isSameAddress, readAndWriteEnabled);
+
+        // Add this collision to the list of collisions for this read operation
+        collisionList.push_back(isCollision);
+      }
+
+      // Check the ReadWrite ports as well
+      for (auto readWriteOp : readWriteOps) {
+        auto isSameAddress = b.createOrFold<comb::ICmpOp>(
+            comb::ICmpPredicate::eq, readOp.getAddress(),
+            readWriteOp.getAddress());
+
+        // If they are the same address, we need to ensure they are going to
+        // collide
+        Value readIsEnabled = readOp.getEnable();
+        Value writeIsEnabled = readWriteOp.getEnable();
+        Value readAndWriteEnabled =
+            b.create<comb::AndOp>(readIsEnabled, writeIsEnabled);
+        Value isCollision =
+            b.create<comb::AndOp>(isSameAddress, readAndWriteEnabled);
+
+        // Add this collision to the list of collisions for this read operation
+        collisionList.push_back(isCollision);
+      }
+
+      if (collisionList.empty()) {
+        continue;
+      }
+
+      // Use createOrFold in case there is only one collision to avoid
+      // unnecessary logic
+      Value conflictTrue = b.createOrFold<comb::OrOp>(mlir::ValueRange(collisionList), false);
+
+      // Random name creation
+      auto symbolicName =
+          symbolNamespace.newName("randomValueForUndefinedBehavior");
+      // Aka choice, but used differently in application
+      auto randomSymbolicName = verif::SymbolicValueOp::create(
+          b, currentResult.getType(), b.getStringAttr(symbolicName));
+      Value randomVal = randomSymbolicName.getResult();
+
+      // If true, we have a read-write collision and we can enable undefined
+      // memory behavior This mux chooses between the correct value and an
+      // undefined value based on whether there is a collision or not This also
+      // upholds the OOB and return the random OOB value if we are out of bounds
+      // regardless of collisions
+      Value mux = b.create<comb::MuxOp>(conflictTrue, randomVal, currentResult);
+
+      Operation *muxOp = mux.getDefiningOp();
+      readOp.getResult().replaceAllUsesExcept(mux, muxOp);
+    }
+
+    // Write-Write Conflict: go through all the writes, see if they are the same
+    // address
+
+    // Loop through all the read and write ports and check if they are accessing
+    // the same address
+
+    /*
+    for (auto writeOp : writeOps) {
+      ImplicitLocOpBuilder b(module.getLoc(), module.getBody());
+      b.setInsertionPointAfter(writeOp);
+
+      // Out of Bounds checker
+      Value currentResult = writeOp.getResult();
+      // Width of the address?
+      // Width greater than supported?
+      uint64_t depth = instance.memOp.getMemory().getType().getDepth();
+
+      // Check if incorrect address
+
+      // I don't think this is needed for writeOp
+      // however, I think it is important to check if it is out of bounds writes
+      // but what do we do then?
+      // TODO
+
+      if (depth > 0) {
+        Value addr = writeOp.getAddress();
+        Value depthValue = b.create<hw::ConstantOp>(addr.getType(), depth);
+
+        // Hazard if: (Address >= Depth) which means we are out of bounds and
+        // can have undefined behavior Use a symbolic value so at runtime the
+        // value is chosen nondeterministically
+        Value isOutOfBounds = b.createOrFold<comb::ICmpOp>(
+            comb::ICmpPredicate::uge, addr, depthValue);
+
+        // Randomize if needed
+        auto oobName = symbolNamespace.newName("randomValueForOOBWrite");
+
+        // Aka choice, but used differently in application
+        auto randomSymbolicOOB = verif::SymbolicValueOp::create(
+            b, currentResult.getType(), b.getStringAttr(oobName));
+        Value randomOOBVal = randomSymbolicOOB.getResult();
+
+        Value muxForOOB =
+            b.create<comb::MuxOp>(isOutOfBounds, randomOOBVal, currentResult);
+        Operation *muxOOBOp = muxForOOB.getDefiningOp();
+        currentResult.replaceAllUsesExcept(muxForOOB, muxOOBOp);
+
+        // Update currentResult so later logic uses the OOB-protected value.
+        currentResult = muxForOOB;
+      }
+
+      // Maintain a list of the actual collisions that we can later use in the
+      // mux.
+      SmallVector<Value, 16> collisionList;
+
+      // Start at the write in the list AFTEr the current write, so no repeats.
+      // TODO: how does this look?
+      auto startIt = llvm::find(writeOps, writeOp);
+
+      for (auto it = startIt; it != writeOps.end(); ++it) {
+        auto writeOp2 = *it;
+        auto isSameAddress =
+            b.create<comb::ICmpOp>(comb::ICmpPredicate::eq,
+                                   writeOp.getAddress(), writeOp2.getAddress());
+
+        // If they are the same address, we need to ensure they are going to
+        // collide
+        Value writeIsEnabled = writeOp.getEnable();
+        Value write2IsEnabled = writeOp2.getEnable();
+        Value bothWritesEnabled =
+            b.create<comb::AndOp>(writeIsEnabled, write2IsEnabled);
+        Value isCollision =
+            b.create<comb::AndOp>(isSameAddress, bothWritesEnabled);
+
+        // Add this collision to the list of collisions for this write operation
+        collisionList.push_back(isCollision);
+      }
+
+      // Check the ReadWrite ports as well
+      for (auto readWriteOp : readWriteOps) {
+        auto isSameAddress = b.createOrFold<comb::ICmpOp>(
+            comb::ICmpPredicate::eq, readOp.getAddress(),
+            readWriteOp.getAddress());
+
+        // If they are the same address, we need to ensure they are going to
+        // collide
+        Value readIsEnabled = readOp.getEnable();
+        Value writeIsEnabled = readWriteOp.getEnable();
+        Value readAndWriteEnabled =
+            b.create<comb::AndOp>(readIsEnabled, writeIsEnabled);
+        Value isCollision =
+            b.create<comb::AndOp>(isSameAddress, readAndWriteEnabled);
+
+        // Add this collision to the list of collisions for this read operation
+        collisionList.push_back(isCollision);
+      }
+
+      if (collisionList.empty()) {
+        continue;
+      }
+
+      // I'm not sure how we make a random "write" value. How do we write
+      // Use createOrFold in case there is only one collision to avoid
+      // unnecessary logic
+      Value conflictTrue =
+          b.createOrFold<comb::OrOp>(mlir::ValueRange(collisionList), false);
+
+      // Random name creation
+      auto symbolicName =
+          symbolNamespace.newName("randomValueForUndefinedBehaviorWrite");
+      // Aka choice, but used differently in application
+      auto randomSymbolicName = verif::SymbolicValueOp::create(
+          b, currentResult.getType(), b.getStringAttr(symbolicName));
+      Value randomVal = randomSymbolicName.getResult();
+
+      // If true, we have a read-write collision and we can enable undefined
+      // memory behavior This mux chooses between the correct value and an
+      // undefined value based on whether there is a collision or not This also
+      // upholds the OOB and return the random OOB value if we are out of bounds
+      // regardless of collisions
+      Value mux = b.create<comb::MuxOp>(conflictTrue, randomVal, currentResult);
+
+      Operation *muxOp = mux.getDefiningOp();
+      writeOp.getResult().replaceAllUsesExcept(mux, muxOp);
+    } */
+  }
 }
--- test/Dialect/Seq/firmem-undefined-behavior.mlir
+++ test/Dialect/Seq/firmem-undefined-behavior.mlir
@@ -4,21 +4,73 @@
 // look for circt-opt
 
 // circt-opt
-// 
+// https://github.com/chipsalliance/firrtl/blob/1.6.x/src/test/scala/firrtl/backends/experimental/smt/random/UndefinedMemoryBehaviorSpec.scala
 
-// CHECK-LABEL: hw.module @ReadWriteConflict
-// CHECK: %mem = seq.firmem 0, 1, undefined, undefined : <12 x 20>
-// CHECK: %0
-// CHECK: %1 = comb.and %enable, %enable : i1
-// CHECK: %2 = comb.and 
 
-// Need case where read out of bounds
+// //CHECK-LABEL: hw.module @ReadWriteConflict // do i check isOutOfBounds, muxForOOB as well?
+// //CHECK: %mem = seq.firmem 0, 1, undefined, undefined : <12 x 20>
+// //CHECK: %0 =  comb.icmp eq %addr, %addr : i4 // same address
+// //CHECK: %1 = comb.and %enable, %enable : i1  // read and write enabled
+// //CHECK: %2 = comb.and %enable, %enable : i1                 // is collision
 
-hw.module @ReadWriteConflict(in %addr: i4, in %clock: !seq.clock, in %enable: i1, out z: i20) {
+
+
+
+// The addresses are equal
+// //CHECK-LABEL: hw.module @ReadWriteConflict
+// //CHECK-NEXT: [[TMP0:%.+]] = comb.icmp eq %addr, %addr : i4 // Same address
+// //CHECK-NEXT: [[TMP1:%.+]] = comb.and  %enable, %enable :i1 // both enabled
+// //CHECK-NEXT: [[TMP2:%.+]] = comb.and [[TMP0]], [[TMP1]] : i1 // Is Collision
+// In the code we make an OR operator: is this needed?
+// //CHECK-NEXT: [[TMP3:%.+]] = SymbolicValue.op// Random Value
+// //CHECK-NEXT: []TMP4:%.+]] = comb.muxOp [[TMP2]], ?? ??
+
+//hw.module @ReadWriteConflict(in %addr: i4, in %clock: !seq.clock, in %enable: i1, out z: i20) {
+//  %mem = seq.firmem 0, 1, undefined, undefined : <12 x 20>
+
+//  %0 = seq.firmem.read_port %mem[%address], clock %clock enable %enable: <12 x 20>
+//  seq.firmem.write_port %mem[%address] = %data, clock %clock enable %enable: <12 x 20>
+
+//  hw.output %0 : i20
+//}
+
+
+
+// //CHECK-LABEL: hw.module @ManyAssertsAndAssumes
+// //CHECK-NEXT:   %c1_i42 = hw.constant 1 : i42
+// //CHECK-NEXT:   [[TMP0:%.+]] = comb.shl %a, %c1_i42 : i42
+// //CHECK-NEXT:   %c2_i42 = hw.constant 2 : i42
+// //CHECK-NEXT:   [[TMP1:%.+]] = comb.icmp ult %a, %c2_i42 : i42
+// //CHECK-NEXT:   %c0_i42 = hw.constant 0 : i42
+// //CHECK-NEXT:   [[TMP2:%.+]] = comb.icmp uge %a, %c0_i42 : i42
+// //CHECK-NEXT:   [[REQ:%.+]] = comb.and [[TMP1]], [[TMP2]] : i1
+// //CHECK-NEXT:   verif.assume [[REQ]] : i1
+// //CHECK-NEXT:   [[TMP3:%.+]] = comb.mul %a, %c2_i42 : i42
+// //CHECK-NEXT:   [[TMP4:%.+]] = comb.icmp eq [[TMP0]], [[TMP3]] : i42
+// //CHECK-NEXT:   [[TMP5:%.+]] = comb.add %a, %a : i42
+// //CHECK-NEXT:   [[TMP6:%.+]] = comb.icmp eq [[TMP0]], [[TMP5]] : i42
+// //CHECK-NEXT:   [[ENS:%.+]] = comb.and [[TMP4]], [[TMP6]] : i1
+// //CHECK-NEXT:   verif.assert [[ENS]] : i1
+// //CHECK-NEXT:   hw.output [[TMP0]] : i42
+// //CHECK-NEXT: }
+
+
+
+
+// Check that out of bounds variable is active
+// Check that the data is random
+
+// CHECK-LABEL: hw.module @ReadWriteConflict // do i check isOutOfBounds, muxForOOB as well?
+// CHECK-NEXT: %mem = seq.firmem 0, 1, undefined, undefined : <12 x 20> // Check that memory exists
+// CHECK-NEXT: [[TMP0:%.+]]  = hw.constant 12 : i64 // Depth: Needs to exist, because depth may not be constant across systems
+// CHECK-NEXT: [[TMP1:%.+]]  = comp.icmp gt  %addr, [[TMP0]] : i4 // Check that the address is no longer within the depth (m_r_oob in scala)
+// CHECK-NEXT: [[TMP2:%.+]]  = SymbolicValue.op  // RANDOM DATA is generated
+// CHECK-NEXT: [[TMP3:%.+]]  = comb.muxOp [[TMP1]], [[TMP2]], {{.*}}: i4 // RANDOM data is chosen. Do we need this? How do we choose the "old" data - is there a vague way to to it?
+hw.module @ReadOutOfBounds(in %addr: i4, in %clock: !seq.clock, in %enable: i1, out z: i20) {
   %mem = seq.firmem 0, 1, undefined, undefined : <12 x 20>
 
-  %0 = seq.firmem.read_port %mem[%address], clock %clock enable %enable: <12 x 20>
-  seq.firmem.write_port %mem[%address] = %data, clock %clock enable %enable: <12 x 20>
+  // How do I ensure that this is our of bounds? Is this accurate?
+  %0 = seq.firmem.read_port %mem[13 * %address], clock %clock enable %enable: <12 x 20>
 
   hw.output %0 : i20
 }
